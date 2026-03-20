32952d803e2a49560b79cde3f9769f37f4a274a0
fantasyfaiz <faizahme@usc.edu>
2026-03-20 00:15:29 -0500

    fix: import InspireSection

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { MihrabHero } from '@/components/mihrab-hero'
 import { ResourcesSection } from '@/components/resources-section'
+import { InspireSection } from '@/components/inspire-section'
 
 export default function Home() {
   return (
